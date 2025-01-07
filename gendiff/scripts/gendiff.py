f8af19d7f5493bc748ff845aead4469af2ca6a47
skaym00t <cerber.lv.23@gmail.com>
2025-01-08 02:26:37 +0400

    added optional -f --format to select the output format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,9 +3,12 @@ import argparse
 parser = argparse.ArgumentParser(prog='gendiff', usage='Compares two configuration files and shows a difference.')
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+parser.add_argument('-f', '--format', help='set format of output')
+    
 
 def main():
     parser.parse_args()
+        
 
 if __name__ == '__main__':
     main()
