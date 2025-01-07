c5ac9187ecf23cea69530c850c5c103dc427421e
skaym00t <cerber.lv.23@gmail.com>
2025-01-08 01:27:17 +0400

    Created the project package and the entry point "gendiff"

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import argparse
+parser = argparse.ArgumentParser(prog='gendiff', usage='Compares two configuration files and shows a difference.')
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+
+def main():
+    parser.parse_args()
+
+if __name__ == '__main__':
+    main()
