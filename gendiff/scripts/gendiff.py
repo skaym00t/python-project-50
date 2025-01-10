81086cf5209df9457e205005d68d71faf4ecdbfb
skaym00t <cerber.lv.23@gmail.com>
2025-01-10 18:35:55 +0400

    parser rebase

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-import argparse
-
-parser = argparse.ArgumentParser(
-    prog="gendiff",
-    usage="Compares two configuration files and shows a difference."
-)
-parser.add_argument("first_file")
-parser.add_argument("second_file")
-parser.add_argument("-f", "--format", help="set format of output")
+from gendiff import parser
 
 
 def main():
