f6ef70f138dbc4c672f494014aced06dc7fe4bb6
v_lazarenko <skaymoot@gmail.com>
2025-01-14 01:38:32 +0300

    feat(gendiff): update main script to use UV for dependency management

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python3
-
-from gendiff import file1, file2, generate_diff, parser
-
+import json
+import os
+from gendiff import generate_diff, parse_args
 
 def main():
-    parser.parse_args()
-    print(generate_diff(file1, file2))
+    args = parse_args()
+    path1 = os.path.abspath(args.first_file)
+    path2 = os.path.abspath(args.second_file)
 
+    with open(path1) as f1, open(path2) as f2:
+        file1 = json.load(f1)
+        file2 = json.load(f2)
+
+    print(generate_diff(file1, file2))
 
 if __name__ == "__main__":
     main()
