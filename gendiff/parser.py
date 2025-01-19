25b8deabdc1197bbcfe4fa2bf5525ff41b030bf7
skaym00t <cerber.lv.23@gmail.com>
2025-01-19 22:12:58 +0400

    feat(parser): Add command-line argument for selecting output format

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -7,9 +7,9 @@ def parse_args():
         prog="gendiff",
         description="Compares two configuration files and shows a difference.",
     )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
+    parser.add_argument("first_file", help="Path to the first file")
+    parser.add_argument("second_file", help="Path to the second file")
     parser.add_argument(
-        "-f", "--format", default="stylish", help="set format of output"
-    )
+        "-f", "--format", choices=['stylish', 'plain'], 
+        default='stylish', help="set format of output (default: stylish)")
     return parser.parse_args()
